a6977fbf496b70d2b8f1612af4e4731e16564cf2
Wiam_Laarabi <wiamlaarabi2001@gmail.com>
2023-11-03 21:18:22 +0100

    Exercice 1 : analyse d'un post tout simple

---
+++ src/main/java/fr/laarabi/tpprgwiam/jsf/Bean.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package fr.laarabi.tpprgwiam.jsf;
+
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+
+@Named(value = "bean")
+@RequestScoped
+public class Bean {
+
+    private int nombre;
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Integer> getNombresSuivants() {
+        int nb = 5;
+        List<Integer> l = new ArrayList<>(nb);
+        for (int i = nombre; i < nombre + nb; i++) {
+            l.add(i);
+        }
+        return l;
+    }
+}
